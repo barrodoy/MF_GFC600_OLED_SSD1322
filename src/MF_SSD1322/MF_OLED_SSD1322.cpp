b08784be50bb7aa0f99942a0edcf20e0d202118f
barrodoy <bar.rodoy@gmail.com>
2022-09-23 18:11:58 +0300

    Improved logic

--- src/MF_SSD1322/MF_OLED_SSD1322.cpp
+++ src/MF_SSD1322/MF_OLED_SSD1322.cpp
@@ -10,22 +10,8 @@ void MF_OLED_SSD1322::begin()
     oled.setFont(u8g2_font_profont22_mf); // choose a suitable font
     oled.drawStr(5, 10, "Welcome to MF"); // write something to the internal memory
     oled.sendBuffer();
-    oled.clearBuffer();
-
-    // loading screen
-    int i = 0;
-    for (i = 0; i < 10; i++) {
-        oled.drawStr(100, 15, "GFC600");
-        oled.sendBuffer();
-        delay(500);
-    }
-
-    refresh();
-    oled.drawStr(100, 15, "Bar");
-    oled.sendBuffer();
     delay(1000);
-    refresh();
-    oled.drawStr(100, 25, cmdMessenger.readStringArg());
+    oled.clearBuffer();
 }
 
 void MF_OLED_SSD1322::refresh()
@@ -57,7 +43,7 @@ void MF_OLED_SSD1322::drawRol()
 
 void MF_OLED_SSD1322::drawHdg()
 {
-    oled.setFont(u8g2_font_courB12_tf);
+    oled.setFont(u8g2_font_12x6LED_tf);
     oled.drawStr(6, 15, "HDG");
 } // end of drawHdg method
 
@@ -149,102 +135,6 @@ void MF_OLED_SSD1322::drawBigGps()
     oled.drawStr(6, 15, "GPS");         // if ALT mode is on, print "ALT"
 } // end of drawBigGps method
 
-String MF_OLED_SSD1322::prepareData(const char *string)
-{
-    String mf_data = string;
-
-    return mf_data;
-
-} // end of prepareData method
-
-bool MF_OLED_SSD1322::fdOn()
-{
-    if (mf_data.charAt(0) == '1') {
-        return true;
-    }
-
-    else {
-        return false;
-    }
-} // end of fdOn method
-
-bool MF_OLED_SSD1322::hdgOn()
-{
-    if (mf_data.charAt(2) == '1') {
-        return true;
-    }
-
-    else {
-        return false;
-    }
-} // end of hdgOn method
-
-bool MF_OLED_SSD1322::vsOn()
-{
-    if (mf_data.charAt(4) == '1') {
-        return true;
-    }
-
-    else {
-        return false;
-    }
-} // end of vsOn method
-
-bool MF_OLED_SSD1322::iasOn()
-{
-    if (mf_data.charAt(12) == '1') {
-        return true;
-    }
-
-    else {
-        return false;
-    }
-} // end of iasOn method
-
-bool MF_OLED_SSD1322::altOn()
-{
-    if (mf_data.charAt(32) == '1') {
-        return true;
-    }
-
-    else {
-        return false;
-    }
-} // end of altOn method
-
-bool MF_OLED_SSD1322::lvlOn()
-{
-    if (mf_data.charAt(34) == '1') {
-        return true;
-    }
-
-    else {
-        return false;
-    }
-} // end of lvlOn method
-
-bool MF_OLED_SSD1322::vorOn()
-{
-    if (mf_data.charAt(36) == '1') {
-        return true;
-    }
-
-    else {
-        return false;
-    }
-} // end of vorOn method
-
-bool MF_OLED_SSD1322::gpsNavOn()
-{
-    if (mf_data.charAt(38) == '1') {
-        return true;
-    }
-
-    else {
-        return false;
-    }
-} // end of gpsNavOn method
-
 void MF_OLED_SSD1322::flash(const char *modeName)
 {
     int i = 0;
@@ -261,196 +151,42 @@ void MF_OLED_SSD1322::flash(const char *modeName)
     }
 }
 
-void MF_OLED_SSD1322::display(const char *string)
+void MF_OLED_SSD1322::display(char *string)
 {
-    // prepare data
 
-    bool fdMode  = fdOn();
-    bool hdgMode = hdgOn();
-    bool vsMode  = vsOn();
-    bool iasMode = iasOn();
-    bool altMode = altOn();
-    bool lvlMode = lvlOn();
-    bool vorMode = vorOn();
-    bool gpsMode = gpsNavOn();
+    int   ap        = atoi(strtok(string, "|")); // MF string - #
+    int   fd        = atoi(strtok(NULL, "|"));   // MF string - !
+    int   yd        = atoi(strtok(NULL, "|"));   // MF string - ?
+    int   hdg       = atoi(strtok(NULL, "|"));   // MF string - @
+    int   nav       = atoi(strtok(NULL, "|"));   // MF string - A
+    int   apr       = atoi(strtok(NULL, "|"));   // MF string - B
+    int   bc        = atoi(strtok(NULL, "|"));   // MF string - C
+    int   vs        = atoi(strtok(NULL, "|"));   // MF string - D
+    char *vsValStr  = strtok(NULL, "|");         // //MF string - E. vs value string for display
+    int   vsVarInt  = atoi(vsValStr);            // vs value as int for calculations
+    int   ias       = atoi(strtok(NULL, "|"));   // MF string - F
+    char *iasValStr = strtok(NULL, "|");         // //MF string - G. ias value string for display
+    int   iasVarInt = atoi(iasValStr);           // ias value as int for calculations
+    int   alt       = atoi(strtok(NULL, "|"));   // MF string - H
+    char *altValStr = strtok(NULL, "|");         // //MF string - I. vs value string for display
+    int   altVarInt = atoi(altValStr);           // ias value as int for calculations
+    int   lvl       = atoi(strtok(NULL, "|"));   // MF string - J
+    int   rol       = atoi(strtok(NULL, "|"));   // MF string - K
+    int   pit       = atoi(strtok(NULL, "|"));   // MF string - L
 
-    // prepare VS data
-    String vsString = mf_data.substring(6, 11); // takes vs from mf_data String object
-    int    vsInt;
-    vsInt          = vsString.toInt();
-    const char *vs = vsString.c_str(); // converts vsString String object to chars array
+    oled.clearBuffer();
 
-    // prepare IAS data
-    String      iasString = mf_data.substring(14, 17);
-    const char *ias       = iasString.c_str();
-
-    // prepare ALT data
-    String altString = mf_data.substring(20, 25);
-    int    altInt;
-    altInt          = altString.toInt();
-    const char *alt = altString.c_str();
-
-    // prepare AP ALT data:
-    String apAltString = mf_data.substring(27, 31);
-    int    apAltInt;
-    apAltInt          = apAltString.toInt();
-    const char *apAlt = apAltString.c_str();
-
-    oled.clearBuffer(); // clear the internal memory           // transfer internal memory to the display
-
-    // lines drawing
     oled.drawLine(52, 11, 52, 57);   // draws the boundary line of the lateral modes
     oled.drawLine(162, 11, 162, 57); // draws the boundary line of the vertical modes
 
-    // charAt(0) - FD
-    // charAt(2) - HDG
-    // charAt(36) - VOR
-    // charAt(34) - LVL
-    // charAt(4) - VS
-    // charAt(12) - IAS
-    // charAt(32) - ALT
-    // charAt(38) - NAV/GPS
-
-    // check if FD is on
-    if (mf_data.charAt(0) == '1') {
-        // if FD is on
-
-        // lateral modes
-
-        // HDG mode
-        if (mf_data.charAt(2) == '1') {
-            drawHdg();
-
-            if (mf_data.charAt(36) == '1') { // if VOR is armed with HDG On
-                drawSmallVor();
-            }
-
-        } // end of HDG mode
-
-        // NAV Mode
-        else if (mf_data.charAt(36) == '1' && mf_data.charAt(2) == '0' && mf_data.charAt(38) == '0') {
-            drawBigVor();
-        } // end of Nav Mode
-
-        // GPS Nav mode
-        else if (mf_data.charAt(36) == '1' && mf_data.charAt(2) == '0' && mf_data.charAt(38) == '1') {
-            drawBigGps();
-        } // end of GPS Nav mode
-
-        else {
-            drawRol();
-            // oled.drawStr(6, 15, "ROL");
-        }
-
-        // vertical modes
-
-        // LVL mode
-        if (mf_data.charAt(34) == '1') {
-            drawLvlLat();
-            drawLvlVer();
-        } // end of LVL mode
-
-        // VS mode
-        else if (mf_data.charAt(4) == '1' && mf_data.charAt(34) == '0') {
-            drawVs();
-            drawFpm();
-            // oled.drawStr(56, 15, "VS");         // if VS mode is on, print "VS"
-            // oled.setFont(u8g2_font_courB08_tf); // choose a suitable font
-            // oled.drawStr(140, 25, "FPM");       // if VS mode is on print "FPM" in a smaller font below the VS value display
-            // oled.setFont(u8g2_font_courB12_tf); // choose a suitable font
-
-            // if selected vs value is 3 positive 3 digits
-            if ((vsInt > 0) && (vsInt < 999)) {
-                oled.setFont(u8g2_font_cu12_t_symbols);
-                oled.drawUTF8(122, 15, "↑");
-                oled.setFont(u8g2_font_courB12_tf); // choose a suitable font
-                oled.drawStr(110, 15, vs);
-            }
-
-            // if selected vs value is positive 4 digites
-            else if (vsInt > 999) {
-
-                oled.setFont(u8g2_font_cu12_t_symbols);
-                oled.drawUTF8(110, 15, "↑");
-                oled.setFont(u8g2_font_courB12_tf); // choose a suitable font
-                oled.drawStr(110, 15, vs);
-            }
-
-            // if selected vs value is negative 3 digits
-            else if (vsInt < 0 && vsInt > -999) {
-
-                // remove the minus sign
-                vsString       = mf_data.substring(8, 11);
-                const char *vs = vsString.c_str(); // converts vsString String object to chars array
-
-                oled.setFont(u8g2_font_cu12_t_symbols);
-                oled.drawUTF8(122, 15, "↓");
-                oled.setFont(u8g2_font_courB12_tf); // choose a suitable font
-                oled.drawStr(130, 15, vs);
-
-            }
-
-            else if (vsInt < -999) {
-                // remove the minus sign
-                vsString       = mf_data.substring(7, 11);
-                const char *vs = vsString.c_str(); // converts vsString String object to chars array
-
-                oled.setFont(u8g2_font_cu12_t_symbols);
-                oled.drawUTF8(114, 15, "↓");
-                oled.setFont(u8g2_font_courB12_tf); // choose a suitable font
-                oled.drawStr(122, 15, vs);
-            }
-
-            else if (vsInt == 0) {
-                oled.setFont(u8g2_font_courB12_tf); // choose a suitable font
-                oled.drawStr(110, 15, vs);
-            }
-
-            // oled.drawStr(26, 25, mf_data.substring(6, 11));
-        } // end of VS mode
-
-        // IAS mode
-        else if (mf_data.charAt(12) == '1') {
-            drawIas();
-            // oled.drawStr(56, 15, "IAS"); // if IAS mode is on, print "IAS"
-            oled.drawStr(130, 15, ias);
-
-            drawKts();
-            // oled.setFont(u8g2_font_courB08_tf); // choose a suitable font
-            // oled.drawStr(140, 25, "KTS");       // if IAS mode is on, print "KTS"
-            // oled.setFont(u8g2_font_courB12_tf); // choose a suitable font
-
-        } // end of IAS mode
-
-        else if (mf_data.charAt(32) == '1') {
-            if (apAltInt - altInt >= 50 || apAltInt - altInt <= -50) {
-                drawBigAlts();
-                oled.drawStr(120, 15, apAlt);
-                drawSmallFt();
-                drawSmallAlt();
-            } // end of "ALTS" if
-
-            else {
-                drawBigAlt();
-                oled.drawStr(120, 15, apAlt);
-                drawSmallFt();
-                drawSmallAlts();
-            } // end of "ALT"
-
-        } // end of ALTS\ALT modes
-
-        else // PIT Mode
-        {
-            oled.drawStr(56, 15, "PIT"); // if PIT mode is on, print "PIT"
-            drawSmallAlts();
-        } // end of "PIT"
-
-        // ALTS text condition
-        if ((mf_data.charAt(4) == '1' || mf_data.charAt(12) == '1') && mf_data.charAt(34) == '0') {
-            drawSmallAlts();
-        }
-
-    } // end of FD if
+    /*
+    VERTICAL MODES DISPLAY
+    */
+    if (vs) {
+        drawVs();
+        oled.drawStr(90, 15, vsValStr);
+        drawFpm();
+    }
 
     // push data to display
     oled.sendBuffer();
